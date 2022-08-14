9df963bd24dc104708dc8f0f2e03ca20f838016e
LeoHong123456 <leo498772018@gmail.com>
2022-08-14 23:46:56 +0800

    first commit

---
+++ src/main/java/com/app/wechat/MultiWechatApplication.java
@@ -0,0 +1,14 @@
+package com.app.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiWechatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MultiWechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/wechat/MultiWechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiWechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
